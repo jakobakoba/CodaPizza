6affd8d5d72829ccd7f0826570dd47721dc5a987
jakobakoba <jakobakoba@gmail.com>
2023-08-28 09:40:56 +0600

    initial

---
+++ app/src/main/java/com/bor96dev/codapizza/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.bor96dev.codapizza
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.bor96dev.codapizza.ui.ToppingCell
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       setContent {
+           ToppingCell()
+           
+       }
+    }
+
+
+}
