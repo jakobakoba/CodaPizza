25c3de2f8d0d6e06000d113ef96f309192e6e87d
jakobakoba <jakobakoba@gmail.com>
2023-08-29 18:52:11 +0600

    dialog set up

--- app/src/main/java/com/bor96dev/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/com/bor96dev/codapizza/ui/PizzaBuilderScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -20,14 +21,13 @@ import androidx.compose.ui.unit.dp
 import com.bor96dev.codapizza.R
 import com.bor96dev.codapizza.model.Pizza
 import com.bor96dev.codapizza.model.Topping
-import com.bor96dev.codapizza.model.ToppingPlacement
 import java.text.NumberFormat
 
 @Composable
 fun PizzaBuilderScreen(
     modifier: Modifier = Modifier,
 ) {
-    var pizza by remember { mutableStateOf(Pizza()) }
+    var pizza by rememberSaveable { mutableStateOf(Pizza()) }
     Column(modifier = modifier) {
         ToppingsList(
             pizza = pizza,
@@ -53,6 +53,14 @@ private fun ToppingsList(
     onEditPizza: (Pizza) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var showToppingPlacementDialog by rememberSaveable { mutableStateOf(false) }
+    if (showToppingPlacementDialog) {
+        ToppingPlacementDialog(
+            onDismissRequest = {
+                showToppingPlacementDialog = false
+            }
+        )
+    }
 
     LazyColumn(modifier = modifier) {
         items(Topping.values()) { topping ->
@@ -62,17 +70,7 @@ private fun ToppingsList(
                 topping = topping,
                 placement = pizza.toppings[topping],
                 onClickTopping = {
-                    var isOnPizza = pizza.toppings[topping] != null
-                    onEditPizza(
-                        pizza.withTopping(
-                            topping = topping,
-                            placement = if (isOnPizza) {
-                                null
-                            } else {
-                                ToppingPlacement.All
-                            }
-                        )
-                    )
+                    showToppingPlacementDialog = true
                 })
         }
     }
@@ -85,7 +83,7 @@ private fun OrderButton(
     modifier: Modifier = Modifier,
 ) {
     Button(onClick = { /*TODO*/ }, modifier = modifier) {
-        val currencyFormatter = NumberFormat.getCurrencyInstance()
+        val currencyFormatter = remember { NumberFormat.getCurrencyInstance() }
         val price = currencyFormatter.format(pizza.price)
         Text(
             text = stringResource(
